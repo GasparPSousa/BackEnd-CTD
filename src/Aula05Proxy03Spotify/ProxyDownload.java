b0f942dc0cda94f60998b8505d36b57f29d64ed8
GasparPSousa <gasparufrj@gmail.com>
2021-10-21 11:45:31 -0300

    Ajuste fino no projeto Spotify

--- src/Aula05Proxy03Spotify/ProxyDownload.java
+++ src/Aula05Proxy03Spotify/ProxyDownload.java
@@ -5,7 +5,9 @@ public class ProxyDownload implements IServicoDownload {
     @Override
     public void baixarMusica(Usuario usuario) {
         if(verificarUsuario(usuario.getTipoDeUsuario())) {
-            System.out.println(usuario.getNome() + ", você é cliente Premium!! Baixe sua música preferida e se divirta!");
+//            System.out.println(usuario.getNome() + ", você é cliente Premium!! Baixe sua música preferida e se divirta!");
+            ServicoDownload servico = new ServicoDownload();
+            servico.baixarMusica(usuario);
         } else {
             System.out.println(usuario.getNome() + ", você é cliente 'Free'. Assine 'Premium' para poder baixar.");
         }
