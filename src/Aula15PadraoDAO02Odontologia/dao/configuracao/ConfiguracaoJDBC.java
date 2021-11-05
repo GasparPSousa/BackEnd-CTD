25a0ca954e671b91e994101d4ed25fe81ade2428
GasparPSousa <gasparufrj@gmail.com>
2021-11-05 10:01:50 -0300

    Criando a classe ConfiguracaoJDBC

--- src/Aula15PadraoDAO02Odontologia/dao/configuracao/ConfiguracaoJDBC.java
+++ src/Aula15PadraoDAO02Odontologia/dao/configuracao/ConfiguracaoJDBC.java
@@ -1,4 +1,46 @@
 package Aula15PadraoDAO02Odontologia.dao.configuracao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConfiguracaoJDBC {
+
+    private String jdbcDriver;
+    private String dbUrl;
+    private String nomeUsuario;
+    private String senhaUsuario;
+
+    // Construtor com todos os parâmetros
+    public ConfiguracaoJDBC(String jdbcDriver, String dbUrl, String nomeUsuario, String senhaUsuario) {
+        this.jdbcDriver = jdbcDriver;
+        this.dbUrl = dbUrl;
+        this.nomeUsuario = nomeUsuario;
+        this.senhaUsuario = senhaUsuario;
+    }
+
+    public ConfiguracaoJDBC() {
+        this.jdbcDriver = "org.h2.Driver";
+        this.dbUrl = "jdbc:h2:~/clinica;DB_CLOSE_DELAY=-1;INIT=RUNSCRIPT FROM 'CREATE1.SQL'";
+        this.nomeUsuario = "sa";
+        this.senhaUsuario = "";
+
+    }
+
+    // Método para conectar no DB
+    public Connection conectarComBancoDeDados() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                    dbUrl,
+                    nomeUsuario,
+                    senhaUsuario
+            );
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
