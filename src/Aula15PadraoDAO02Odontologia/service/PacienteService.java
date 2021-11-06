373bad0c70da0e571a8b89cb5a611120b2556bc3
GasparPSousa <gasparufrj@gmail.com>
2021-11-06 11:06:00 -0300

    Criando a classe PacienteService

--- src/Aula15PadraoDAO02Odontologia/service/PacienteService.java
+++ src/Aula15PadraoDAO02Odontologia/service/PacienteService.java
@@ -1,4 +1,33 @@
 package Aula15PadraoDAO02Odontologia.service;
 
+import Aula15PadraoDAO02Odontologia.dao.impl.IDao;
+import Aula15PadraoDAO02Odontologia.model.Paciente;
+
+import java.util.List;
+import java.util.Optional;
+
 public class PacienteService {
+
+    private IDao<Paciente> pacienteIDao;
+
+    public PacienteService(IDao<Paciente> pacienteIDao) {
+        this.pacienteIDao = pacienteIDao;
+    }
+
+    public Paciente salvar(Paciente paciente) {
+        pacienteIDao.salvar(paciente);
+        return paciente;
+    }
+
+    public Optional<Paciente> buscar(Integer id) {
+        return pacienteIDao.buscar(id);
+    }
+
+    public List<Paciente> buscarTodos() {
+        return pacienteIDao.buscarTodos();
+    }
+
+    public void excluir(Integer id) {
+        pacienteIDao.exluir(id);
+    }
 }
